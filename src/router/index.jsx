48c2862ac005a3ea294361893c0f496d7ef0dad4
U.amazing <1259642433@qq.com>
2020-06-19 12:10:09 +0800

    遍历生成路由

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,25 +3,31 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Error404 from '../pages/Error404'
+
 const routes = [
   {
-    path: "/",
-    component: () => import('../pages/Home'),
-  }
-  // etc.
+    path:'/',
+    component: Home,
+    exact:true
+  },
+  {
+    path:'/home',
+    component: Home,
+    exact:true
+  },
+  {
+    path:'',
+    component: Error404
+  },
 ]
-function index() {
+export default function index() {
     return (
         <Router>
           <Switch>
-            <Route path="/home" component={Home} />
-            <Route exact path="/404" component={Error404} />
-            <Route exact path="/" component={Home} />
-            {/* <Redirect to={"/404"} /> */}
-            <Route component={Error404} />
+            {routes.map(route => (
+              <Route key={route.path} path={route.path} component={route.component}  exact={route.exact} />
+            ))}
           </Switch>
         </Router>
     )  
-  }
-  
-  export default index
+}
